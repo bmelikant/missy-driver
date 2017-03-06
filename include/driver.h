a519c7a4fcd28e7806e8dac8b95cd8615c2366a7
Ben Melikant <bmmelikant@gmail.com>
2017-03-06 10:55:27 -0500

    Created initial driver toolkit files. Created empty files for build and install scripts.

---
+++ include/driver.h
@@ -0,0 +1,13 @@
+/*
+ * driver.h
+ *
+ *  Created on: Mar 6, 2017
+ *      Author: bmelikant
+ */
+
+#ifndef MISSY_DRIVER_DRIVER_H_
+#define MISSY_DRIVER_DRIVER_H_
+
+
+
+#endif /* MISSY_DRIVER_DRIVER_H_ */
---
+++ src/driver.c
@@ -0,0 +1,9 @@
+/*
+ * driver.c
+ *
+ *  Created on: Mar 6, 2017
+ *      Author: bmelikant
+ */
+
+#include <driver.h>
+
